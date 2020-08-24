5d732575e505761d47386124e65cb8fc96865e99
Maheshchandra Hegde <hid.mahesh@gmail.com>
2020-08-25 01:28:30 +0530

    addded bundler

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-let reactWidget = require('/node_modules/hegdemahesh-react-widget-demo/lib/ReactWidgetDemo.js');
+let reactWidget = require('./node_modules/hegdemahesh-react-widget-demo/dist/ReactWidgetDemo.js');
+
+function loadWidget() {
+    //window['ReactWidgetDemo'].initializeWidget();
+    reactWidget.initializeWidget();
+}
+
+
+function clearWidget() {
+    //window['ReactWidgetDemo'].unloadWidget();
+    reactWidget.unloadWidget();
+}
+
+document.querySelector('#loadButton').addEventListener('click', loadWidget);
+document.querySelector('#clearButton').addEventListener('click', clearWidget);
 
